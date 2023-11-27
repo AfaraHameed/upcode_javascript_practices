344a0a5817157c62298db2619661a1fa731c72d1
AfaraHameed <afarasidheek@gmail.com>
2023-11-27 12:51:16 +0530

    updated function validate

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function sumOfTwoNo() {
   var result = document.getElementById("result");
   result.innerText = "sum is:" + add;
 }
-function showMessage(message, color) {
+function showMessage(message, color="gray") {
   var result = document.getElementById("validate_result");
   result.innerText = message;
   result.style.color = color;
@@ -15,9 +15,9 @@ function validate() {
   var place = document.getElementById("place").value;
   var qualification = document.getElementById("qualification").value;
 
-  if (mark >= 50 && place == "kozhikode")
+  if (mark >= 50 && place == "kozhikode"&& qualification == "Btech")
     showMessage("You are eligible to apply job", "green");
   else if ((mark >= 50 || place == "kannur") && qualification == "MBA")
     showMessage("You are eligible to apply job", "green");
-  else showMessage("You are not eligible to apply job", "orange");
+  else showMessage("You are not eligible to apply job");
 }
